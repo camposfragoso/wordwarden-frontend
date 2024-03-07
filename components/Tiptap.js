3d13b637eb06f2d3e0d672561d7daae97d54834d
René <renedechivre@gmail.com>
2024-03-07 14:35:11 +0100

    changed way to handle llm answer highlights

--- components/Tiptap.js
+++ components/Tiptap.js
@@ -7,14 +7,15 @@ import BulletList from '@tiptap/extension-bullet-list';
 import Highlight from '@tiptap/extension-highlight';
 import AssistantMark from './Tiptap_custom_extensions/AssistantMark';
 import { HoverExtension } from './Tiptap_custom_extensions/AddHoverEvent';
+import { HighlightCustom } from './Tiptap_custom_extensions/HighlightCustomExtension';
 
-const MIN_IMPORTANCE = 8;
+const MIN_IMPORTANCE = 1;
 
 const Tiptap = () => {
   const [title, setTitle] = useState('Document title');
   const [content, setContent] = useState('Our planet is getting hotter. Since the Industrial Revolution—an event that spurred the use of fossil fuels in everything from power plants to transportation—Earth has warmed by 1 degree Celsius, about 2 degrees Fahrenheit.  That may sound insignificant, but 2023 was the hottest year on record, and all 10 of the hottest years on record have occurred in the past decade.  Global warming and climate change are often used interchangeably as synonyms, but scientists prefer to use “climate change” when describing the complex shifts now affecting our planet’s weather and climate systems.  Climate change encompasses not only rising average temperatures but also natural disasters, shifting wildlife habitats, rising seas, and a range of other impacts. All of these changes are emerging as humans continue to add heat-trapping greenhouse gases, like carbon dioxide and methane, to the atmosphere.');
 
-  const [llmAnswer, setLlmAnswer] = useState({ "devil": [ { "excerpt": "Since the Industrial Revolution—an event that spurred the use of fossil fuels in everything from power plants to transportation—Earth has warmed by 1 degree Celsius, about 2 degrees Fahrenheit.", "proposition": "The Industrial Revolution's reliance on fossil fuels may not be the sole cause of Earth's temperature increase, as natural climate fluctuations could also play a significant role.", "importance": "8" }, { "excerpt": "2023 was the hottest year on record, and all 10 of the hottest years on record have occurred in the past decade.", "proposition": "The recent spike in global temperatures could be a temporary fluctuation rather than a long-term trend, and other factors such as solar activity might contribute to this increase.", "importance": "7" }, { "excerpt": "Climate change encompasses not only rising average temperatures but also natural disasters, shifting wildlife habitats, rising seas, and a range of other impacts.", "proposition": "The term 'climate change' is overly broad and misleading, as it implies that all these impacts are directly caused by human-induced global warming. Some may be naturally occurring phenomena unrelated to greenhouse gas emissions.", "importance": "6" } ], "sum": [ { "excerpt": "Our planet is getting hotter.", "proposition": "The Earth's temperature is rising.", "importance": "8" }, { "excerpt": "2023 was the hottest year on record, and all 10 of the hottest years on record have occurred in the past decade.", "proposition": "The last decade had the 10 hottest years on record.", "importance": "9" }, { "excerpt": "Global warming and climate change are often used interchangeably as synonyms, but scientists prefer to use “climate change” when describing the complex shifts now affecting our planet’s weather and climate systems.", "proposition": "Scientists differentiate between global warming and climate change.", "importance": "7" }, { "excerpt": "Climate change encompasses not only rising average temperatures but also natural disasters, shifting wildlife habitats, rising seas, and a range of other impacts.", "proposition": "Climate change includes various effects like temperature rise, natural disasters, habitat shifts, and sea level rise.", "importance": "9" }, { "excerpt": "All of these changes are emerging as humans continue to add heat-trapping greenhouse gases, like carbon dioxide and methane, to the atmosphere.", "proposition": "Climate change results from human addition of greenhouse gases.", "importance": "8" } ] });
+  const [llmAnswer, setLlmAnswer] = useState({ "devil": [ { "excerpt": "Our planet is getting hotter.", "proposition": "The Earth's temperature is rising.", "importance": "8" }, { "excerpt": "Since the Industrial Revolution—an event that spurred the use of fossil fuels in everything from power plants to transportation—Earth has warmed by 1 degree Celsius, about 2 degrees Fahrenheit.", "proposition": "The Industrial Revolution's reliance on fossil fuels may not be the sole cause of Earth's temperature increase, as natural climate fluctuations could also play a significant role.", "importance": "8" }, { "excerpt": "2023 was the hottest year on record, and all 10 of the hottest years on record have occurred in the past decade.", "proposition": "The recent spike in global temperatures could be a temporary fluctuation rather than a long-term trend, and other factors such as solar activity might contribute to this increase.", "importance": "7" }, { "excerpt": "Climate change encompasses not only rising average temperatures but also natural disasters, shifting wildlife habitats, rising seas, and a range of other impacts.", "proposition": "The term 'climate change' is overly broad and misleading, as it implies that all these impacts are directly caused by human-induced global warming. Some may be naturally occurring phenomena unrelated to greenhouse gas emissions.", "importance": "6" } ], "sum": [ { "excerpt": "Our planet is getting hotter.", "proposition": "The Earth's temperature is rising.", "importance": "8" }, { "excerpt": "2023 was the hottest year on record, and all 10 of the hottest years on record have occurred in the past decade.", "proposition": "The last decade had the 10 hottest years on record.", "importance": "9" }, { "excerpt": "Global warming and climate change are often used interchangeably as synonyms, but scientists prefer to use “climate change” when describing the complex shifts now affecting our planet’s weather and climate systems.", "proposition": "Scientists differentiate between global warming and climate change.", "importance": "7" }, { "excerpt": "Climate change encompasses not only rising average temperatures but also natural disasters, shifting wildlife habitats, rising seas, and a range of other impacts.", "proposition": "Climate change includes various effects like temperature rise, natural disasters, habitat shifts, and sea level rise.", "importance": "9" }, { "excerpt": "All of these changes are emerging as humans continue to add heat-trapping greenhouse gases, like carbon dioxide and methane, to the atmosphere.", "proposition": "Climate change results from human addition of greenhouse gases.", "importance": "8" } ] });
   
   const [showHighLightMenu, setShowHighlightMenu] = useState(false)
   const [threadDiv, setThreadDiv] = useState(null)
@@ -28,6 +29,9 @@ const Tiptap = () => {
       Highlight.configure({
         multicolor: true,
       }),
+      HighlightCustom.configure({
+        multicolor: true
+      }),
       AssistantMark,
       HoverExtension.configure({
         onMouseOver: (view, event) => {
@@ -37,10 +41,24 @@ const Tiptap = () => {
           // event
         },
         onClick: (view, event) => {
-          const assistant = event.target.getAttribute('assistant');
+
+          const getAllAttributes = (element) => {
+            const attributes = {};
+            for (let i = 0; i < element.attributes.length; i++) {
+              const attr = element.attributes[i];
+              attributes[attr.name] = attr.value;
+            }
+            return attributes;
+          };
+
+          const assistant = getAllAttributes(event.target);
           const proposition = event.target.getAttribute('proposition');
-          const excerpt = event.target.textContent
-          console.log(excerpt)
+          const excerpt = event.target.textContent;
+          console.log(event.target)
+          if (!assistant || !excerpt || !proposition) {
+            setThreadDiv(null)
+            return
+          }
           setThreadDiv({ assistant, excerpt, proposition })
         }
       }),
@@ -86,12 +104,10 @@ const Tiptap = () => {
   // -------------------------------------------------------------------------
   // HANDLE SEND BUTTON -- DEV
   const handleSendClick = async() => {
-    // console.log(content)
     const response = await fetch('http://localhost:3000/mistral', {
       method: 'POST',
       headers: { 'Content-type': 'application/json' },
       body: JSON.stringify({ answer: content }),
-      // user: 1,
     });
     
     const data = await response.json();
@@ -109,16 +125,20 @@ const Tiptap = () => {
   useEffect(() => {
 
     // Function to highlight text
-    const highlightText = (excerpt, color, proposition, assistant) => {
+    const highlightText = (excerpt, content) => {
       const searchText = excerpt;
       editor.state.doc.descendants((node, pos) => {
         if (node.isText && node.text.includes(searchText)) {
           const startIndex = node.text.indexOf(searchText) + pos;
           const endIndex = startIndex + searchText.length;
   
+          const keys = Object.keys(content)
+          let color 
+          keys.length > 1 ? color = `var(--multipleAssistants)` : color = `var(--${keys[0]})`
           editor.chain().focus().setTextSelection({ from: startIndex, to: endIndex })
-            .setMark('assistantMark', { assistant, proposition })
-            // .setMark('propositionMark', { proposition, assistant })
+            .setMark('assistantMark', {
+              propositions: (JSON.stringify(content)),
+            })
             .setHighlight({ color })
             .run();
         }
@@ -127,14 +147,24 @@ const Tiptap = () => {
     
     // Call highlight function for text in llmAnswer
     if (llmAnswer && editor) {
-      Object.entries(llmAnswer).forEach(([assistant, content]) => {
 
-        // Colors => TODO : handle with object inside highlight function
-        let colors = { 'devil': '#E6379F', "sum": '#40E637', 'elaborator': 'lightgreen' }
-        content.forEach(item => {
-          item.importance >= MIN_IMPORTANCE && highlightText(item.excerpt, colors[assistant], item.proposition, assistant);
-        });
-      });
+      let newObj = {};
+      Object.entries(llmAnswer).forEach(([assistant, content]) => {
+        content.forEach(({excerpt, proposition, importance}) => {
+          if (importance >= MIN_IMPORTANCE) {
+            if (!newObj[excerpt]) {
+              newObj[excerpt] = {};
+            }
+            newObj[excerpt][assistant] = proposition;
+          }
+        })
+      })
+
+      Object.entries(newObj).forEach(([excerpt, content]) => {
+        console.log(content)
+        highlightText(excerpt, content)
+      })
+      
     }
   }, [llmAnswer, editor]);
 
@@ -151,9 +181,9 @@ const Tiptap = () => {
           const endIndex = startIndex + searchText.length;
   
           action === 'close' && editor.chain().setTextSelection({ from: startIndex, to: endIndex })
-          .unsetMark('assistantMark')
-          .unsetHighlight()
-          .run();
+            .unsetMark('assistantMark')
+            .unsetHighlight()
+            .run();
 
           action === 'replace' && editor.chain().setTextSelection({ from: startIndex, to: endIndex })
             .unsetMark('assistantMark')
--- components/Tiptap_custom_extensions/AssistantMark.js
+++ components/Tiptap_custom_extensions/AssistantMark.js
@@ -5,30 +5,15 @@ const AssistantMark = Mark.create({
 
   addAttributes() {
     return {
-      assistant: {
+      propositions: {
         default: null,
-        parseHTML: element => element.getAttribute('assistant'),
+        parseHTML: element => element.getAttribute('propositions'),
         renderHTML: attributes => {
-          if (!attributes.assistant) {
+          if (!attributes.propositions) {
             return {}
           }
 
-          return {
-            'assistant': attributes.assistant,
-          }
-        },
-      },
-      proposition: {
-        default: null,
-        parseHTML: element => element.getAttribute('proposition'),
-        renderHTML: attributes => {
-          if (!attributes.proposition) {
-            return {}
-          }
-
-          return {
-            'proposition': attributes.proposition,
-          }
+          return { propositions: attributes.propositions };
         },
       },
     }
@@ -37,14 +22,87 @@ const AssistantMark = Mark.create({
   parseHTML() {
     return [
       {
-        tag: 'span[assistant][proposition]',
+        tag: 'span[propositions]',
       },
     ]
   },
 
   renderHTML({ HTMLAttributes }) {
-    return ['span', mergeAttributes(HTMLAttributes), 0]
+    let props = {};
+    if (HTMLAttributes.propositions) {
+      try {
+        props = JSON.parse(HTMLAttributes.propositions);
+      } catch (e) {
+        console.error("Error parsing propositions attribute:", e);
+      }
+    }
+
+    const dynamicAttributes = {};
+    Object.keys(props).forEach(key => {
+      dynamicAttributes[key] = props[key];
+    });
+
+    return ['span', mergeAttributes(dynamicAttributes), 0];
   },
-})
+});
 
 export default AssistantMark;
+
+
+
+
+
+
+
+
+
+// import { Mark, mergeAttributes } from '@tiptap/core';
+
+// const AssistantMark = Mark.create({
+//   name: 'assistantMark',
+
+//   addAttributes() {
+//     return {
+//       assistant: {
+//         default: null,
+//         parseHTML: element => element.getAttribute('assistant'),
+//         renderHTML: attributes => {
+//           if (!attributes.assistant) {
+//             return {}
+//           }
+
+//           return {
+//             'assistant': attributes.assistant,
+//           }
+//         },
+//       },
+//       proposition: {
+//         default: null,
+//         parseHTML: element => element.getAttribute('proposition'),
+//         renderHTML: attributes => {
+//           if (!attributes.proposition) {
+//             return {}
+//           }
+
+//           return {
+//             'proposition': attributes.proposition,
+//           }
+//         },
+//       },
+//     }
+//   },
+
+//   parseHTML() {
+//     return [
+//       {
+//         tag: 'span[assistant][proposition]',
+//       },
+//     ]
+//   },
+
+//   renderHTML({ HTMLAttributes }) {
+//     return ['span', mergeAttributes(HTMLAttributes), 0]
+//   },
+// })
+
+// export default AssistantMark;
