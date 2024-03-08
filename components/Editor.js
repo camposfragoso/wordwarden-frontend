93c6cfbaf07b05a66db9c3dca479391f614b0d24
Camille <camposfragoso@proton.me>
2024-03-08 10:56:10 +0100

    m

--- components/Editor.js
+++ components/Editor.js
@@ -2,6 +2,12 @@ import Document from '@tiptap/extension-document'
 import Placeholder from '@tiptap/extension-placeholder'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import Underline from '@tiptap/extension-underline';
+import BulletList from '@tiptap/extension-bullet-list';
+import Highlight from '@tiptap/extension-highlight';
+import AssistantMark from './Tiptap_custom_extensions/AssistantMark';
+import { HoverExtension } from './Tiptap_custom_extensions/AddHoverEvent';
+import { HighlightCustom } from './Tiptap_custom_extensions/HighlightCustomExtension';
 import React from 'react'
 import styles from '../styles/Test.module.css';
 import { useState } from 'react'
@@ -16,7 +22,15 @@ export default () => {
   const [llmAnswer, setLlmAnswer] = useState({ "devil": [ { "excerpt": "Our planet is getting hotter.", "proposition": "The Earth's temperature is rising.", "importance": "8" }, { "excerpt": "Since the Industrial Revolution—an event that spurred the use of fossil fuels in everything from power plants to transportation—Earth has warmed by 1 degree Celsius, about 2 degrees Fahrenheit.", "proposition": "The Industrial Revolution's reliance on fossil fuels may not be the sole cause of Earth's temperature increase, as natural climate fluctuations could also play a significant role.", "importance": "8" }, { "excerpt": "2023 was the hottest year on record, and all 10 of the hottest years on record have occurred in the past decade.", "proposition": "The recent spike in global temperatures could be a temporary fluctuation rather than a long-term trend, and other factors such as solar activity might contribute to this increase.", "importance": "7" }, { "excerpt": "Climate change encompasses not only rising average temperatures but also natural disasters, shifting wildlife habitats, rising seas, and a range of other impacts.", "proposition": "The term 'climate change' is overly broad and misleading, as it implies that all these impacts are directly caused by human-induced global warming. Some may be naturally occurring phenomena unrelated to greenhouse gas emissions.", "importance": "6" } ], "sum": [ { "excerpt": "Our planet is getting hotter.", "proposition": "The Earth's temperature is rising.", "importance": "8" }, { "excerpt": "2023 was the hottest year on record, and all 10 of the hottest years on record have occurred in the past decade.", "proposition": "The last decade had the 10 hottest years on record.", "importance": "9" }, { "excerpt": "Global warming and climate change are often used interchangeably as synonyms, but scientists prefer to use “climate change” when describing the complex shifts now affecting our planet’s weather and climate systems.", "proposition": "Scientists differentiate between global warming and climate change.", "importance": "7" }, { "excerpt": "Climate change encompasses not only rising average temperatures but also natural disasters, shifting wildlife habitats, rising seas, and a range of other impacts.", "proposition": "Climate change includes various effects like temperature rise, natural disasters, habitat shifts, and sea level rise.", "importance": "9" }, { "excerpt": "All of these changes are emerging as humans continue to add heat-trapping greenhouse gases, like carbon dioxide and methane, to the atmosphere.", "proposition": "Climate change results from human addition of greenhouse gases.", "importance": "8" } ] });
   const [threadDiv, setThreadDiv] = useState([])
 
-  
+  const getAllAttributes = (element) => {
+    const attributes = {};
+    for (let i = 0; i < element.attributes.length; i++) {
+      const attr = element.attributes[i];
+      attributes[attr.name] = attr.value;
+    }
+    return attributes;
+  };
+
   const editor = useEditor({
     extensions: [
       CustomDocument,
@@ -28,12 +42,66 @@ export default () => {
           if (node.type.name === 'heading') {
             return 'What’s the title?'
           }
-
           return 'Can you add some further context?'
         },
       }),
+      Underline,
+      BulletList,
+      Highlight.configure({
+        multicolor: true,
+      }),
+      HighlightCustom.configure({
+        multicolor: true
+      }),
+      AssistantMark,
+      HoverExtension.configure({
+        onMouseOver: (view, event) => {
+          const attributes = getAllAttributes(event.target);
+          const excerpt = event.target.textContent;
+        
+          Object.entries(attributes).forEach(([assistant, proposition]) => {
+            setThreadDiv(prevThreadDiv => {
+              const index = prevThreadDiv.findIndex(thread => thread.assistant === assistant && thread.proposition === proposition);
+        
+              if (index !== -1) {
+                return prevThreadDiv;
+              } else {
+                return [...prevThreadDiv, { assistant, proposition, excerpt, hover: true, clicked: false }];
+              }
+            });
+          });
+        },
+        onMouseOut: (view, event) => {
+ 
+          const attributes = getAllAttributes(event.target);
+          Object.entries(attributes).forEach(([assistant, proposition]) => {
+            setThreadDiv(currentThreads =>
+              currentThreads.map(thread =>
+                thread.assistant === assistant && thread.proposition === proposition
+                  ? { ...thread, hover: false }
+                  : thread
+              ).filter(thread => thread.clicked || thread.hover)
+            );
+          });
+        },
+        onClick: (view, event) => {
+          const attributes = getAllAttributes(event.target);
+          const excerpt = event.target.textContent;
+        
+          Object.entries(attributes).forEach(([assistant, proposition]) => {
+            setThreadDiv(currentThreads =>
+              currentThreads.map(thread =>
+                thread.assistant === assistant && thread.proposition === proposition
+                  ? { ...thread, clicked: true, excerpt }
+                  : thread
+              )
+            );
+          });
+        }
+        
+      }),
     ],
-    content
+    content,
   })
 
   return (
