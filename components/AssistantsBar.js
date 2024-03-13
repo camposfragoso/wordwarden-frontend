e0052be440e63ea3ee927ebc2c7925c3036cb289
René <renedechivre@gmail.com>
2024-03-13 14:25:45 +0100

    files modal

--- components/AssistantsBar.js
+++ components/AssistantsBar.js
@@ -5,29 +5,31 @@ import { assistantsList } from '../modules/assistantsList';
 function AssistantsBar({ assistants, setAssistantsInBar, activeAssistants, loading }) {
 
   return (
-    <div className={styles.assistantsBar}>
-      <div className={styles.assistants}>
-        {assistantsList.map(({ id, name }) => {
-          return (
-            <div key={id} className={styles.assistantContainer}>
-              <div className={activeAssistants.includes(id) && assistants.includes(id) ? styles.activeAssistantWrapper : styles.inactiveAssistantWrapper}>
-                <button 
-                  onClick={() => setAssistantsInBar(id)} 
-                  className={styles.assistantButton}
-                  style={{backgroundColor: assistants.includes(id) ? `var(--${id})` : 'var(--black)'}}
-                  >
-                  {id}
-                </button>
+    <div className={styles.container}>
+      <div className={styles.assistantsBar}>
+        <div className={styles.assistants}>
+          {assistantsList.map(({ id, name }) => {
+            return (
+              <div key={id} className={styles.assistantContainer}>
+                <div className={activeAssistants.includes(id) && assistants.includes(id) ? styles.activeAssistantWrapper : styles.inactiveAssistantWrapper}>
+                  <button 
+                    onClick={() => setAssistantsInBar(id)} 
+                    className={styles.assistantButton}
+                    style={{backgroundColor: assistants.includes(id) ? `var(--${id})` : 'var(--black)'}}
+                    >
+                    {id}
+                  </button>
+                </div>
               </div>
-            </div>
-          )
-        })}
-        {loading &&
-        <div className={styles.loaderContainer}>
-          <span className={styles.loader}></span>
+            )
+          })}
         </div>
-        }
       </div>
+      {loading &&
+      <div className={styles.loaderContainer}>
+        <span className={styles.loader}></span>
+      </div>
+      }
     </div>
   )
 }
